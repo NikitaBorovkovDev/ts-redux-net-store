229ffff9fc82c8f374dc9353af687375cf94eb9b
Nick <nickfluffybr@gmail.com>
2023-06-12 08:43:20 +0300

    update db

--- src/components/app/appHeader/cart/Cart.tsx
+++ src/components/app/appHeader/cart/Cart.tsx
@@ -1,49 +1,38 @@
-import {useEffect, useRef, useState} from "react";
-import {createPortal} from "react-dom";
-import {useAppSelector} from "hooks/reduxHooks";
-import "./cart.scss";
-import {selectLocalStorageProductsValue} from "components/app/cartSlice";
-import CartSideBar from "../cartSideBar/CartSideBar";
+import { useEffect, useRef, useState } from 'react';
+import { createPortal } from 'react-dom';
+import { useAppSelector } from 'hooks/reduxHooks';
+import './cart.scss';
+import { selectLocalStorageProductsValue } from 'components/app/cartSlice';
+import CartSideBar from '../cartSideBar/CartSideBar';
 
 const Cart = () => {
-    const cartRef = useRef<HTMLSpanElement>(null);
-    const [length, setLength] = useState(0);
-    const [showModalCart, setModalCart] = useState(false);
-    const productsIsStorage = useAppSelector(selectLocalStorageProductsValue);
+	const cartRef = useRef<HTMLSpanElement>(null);
+	const [length, setLength] = useState(0);
+	const [showModalCart, setModalCart] = useState(false);
+	const productsIsStorage = useAppSelector(selectLocalStorageProductsValue);
 
-    useEffect(() => {
-        setLength(productsIsStorage.length);
-    }, [productsIsStorage]);
+	useEffect(() => {
+		setLength(productsIsStorage.length);
+	}, [productsIsStorage]);
 
-    useEffect(() => {
-        if (showModalCart) {
-            document.body.style.overflow = "hidden";
-        } else {
-            document.body.style.overflow = "visible";
-        }
-        return () => {
-            document.body.style.overflow = "visible";
-        };
-    }, [showModalCart]);
-
-    return (
-        <>
-            <span
-                ref={cartRef}
-                className="cart"
-                onClick={() => setModalCart(true)}>
-                <span className="cart__text">{length}</span>
-            </span>
-            {showModalCart &&
-                createPortal(
-                    <CartSideBar
-                        onClose={() => setModalCart(false)}
-                        parentRef={cartRef}
-                    />,
-                    document.body
-                )}
-        </>
-    );
+	return (
+		<>
+			<span
+				ref={cartRef}
+				className="cart"
+				onClick={() => setModalCart(true)}>
+				<span className="cart__text">{length}</span>
+			</span>
+			{showModalCart &&
+				createPortal(
+					<CartSideBar
+						onClose={() => setModalCart(false)}
+						parentRef={cartRef}
+					/>,
+					document.body
+				)}
+		</>
+	);
 };
 
 export default Cart;
